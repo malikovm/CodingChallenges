8df84f5507cdc8bc25d24c04dee68e52af553b1c
Mikayil <mikayil.malikov@gmail.com>
2020-03-18 22:57:30 -0400

    project complete! autocomplete.py now takes the user input and finds all pokemon names that start with those letters

--- autocomplete.py
+++ autocomplete.py
@@ -2,7 +2,18 @@ pokeFile = open("pokemon.csv", "r")
 pokeString = pokeFile.read().strip()
 pokeFile.close()
 filePrint = (input("Do you want to list all pokemon? (Y/N)")).lower()
-if filePrint == "y": print(pokeString)
 pokeList = pokeString.split(", ")
-prefix = input("Enter the first few characters of the desired Pokemon name: ")
+if filePrint == "y": print(*pokeList, sep = "\n")
+prefix = input("Enter the first few letters of the desired Pokemon's name: ").strip()
+prefix = prefix[0].upper() + prefix[1:].lower()
+print("Looking for all Pokemon that start with \"{}\"...".format(prefix))
+prefixLen = len(prefix)
+outputPokelist = []
+for mon in pokeList:
+    if mon[:prefixLen] == prefix: outputPokelist.append(mon)
+matches = len(outputPokelist)
+if matches == 0: print("No Pokemon name starts with \"{}\"".format(prefix))
+else:
+    print(matches, " Pokemon start with \"{}\":".format(prefix))
+    print(*outputPokelist, sep = ", ")
 
