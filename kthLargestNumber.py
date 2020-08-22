03651b22e2bd9bc65d5c6db10607e729777c2f92
Mikayil <mikayil.malikov@gmail.com>
2020-08-21 22:48:39 -0400

    Finished kth largest number problem and updated problems list to reflect this

--- kthLargestNumber.py
+++ kthLargestNumber.py
@@ -1,4 +1,15 @@
-numList = []
-index = 3
+numList = [5, 10, 8, 6, 3, 2, 9, 1, 4, 7, 12, 13]
+index = 8
 
+trackerList = [numList[0]]
+
+for i in range(len(numList)):
+    for j in range(len(trackerList)):
+        if numList[i]>trackerList[j] and len(trackerList)<index and numList[i] not in trackerList:
+            trackerList.insert(j,numList[i])
+        elif numList[i]>trackerList[j] and numList[i] not in trackerList:
+            trackerList.pop()
+            trackerList.insert(j, numList[i])
+
+print("The {}th largest number in the list is: {}".format(index, trackerList[index-1]))
 
